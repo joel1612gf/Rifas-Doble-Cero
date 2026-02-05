b608175e3f7512bbf96229138699bc3eacb0a163
joel1612gf <joel110gf@gmail.com>
2026-02-05 16:43:31 -0400

    nuevo numero

--- main.js
+++ main.js
@@ -1463,7 +1463,7 @@ document.addEventListener('keydown', (e) => {
 });
 // === Contacto por WhatsApp (sin backend) ===
 // Cambia este número si hace falta: en formato internacional SIN "+"
-const CONTACT_WA = '584241242291';
+const CONTACT_WA = '584221756187';
 
 function contactToWhatsApp() {
   const nameEl = document.getElementById('name');
